ba5e06e31fcaa5e1727f1a0c1e7ae0ae5c7f1009
Gabriela Zalta <gzalta96@gmail.com>
2020-11-28 12:08:59 -0500

    inital setup and question array created

---
+++ assets/js/quiz.js
@@ -0,0 +1,33 @@
+//quiz questions
+var questions= [
+    {
+        q: "Are you experiencing any of the following symptoms: severe difficulty breathing, severe chest pain, feeling confused or unsire of where you are, losing consciousness ",
+        ch: ["Yes", "No"],
+        a: "Yes"
+    },
+    {
+        q: "Are you feeling sick?",
+        ch: ["Yes", "No"],
+        a: "Yes"
+    },
+    {
+        q: "In the two weeks before you felt sick, did you care for or have close contact (within 6 feet of an infected person for at least 15 minutes) with someone with symptoms of COVID-19, tested for COVID-19, or diagnosed with COVID-19?",
+        ch: ["Yes", "No"],
+        a: "Yes"
+    },
+    {
+        q: "Do you have any of the following: Fever, Cough, Mild or moderate dificulty breathing, Sore throat, Body ache, Vomiting or diarrhea, New loss of taste or smell, congestion or runny nose, other symptoms",
+        ch: ["Yes", "No"],
+        a: "Yes"
+    },
+    {
+        q: "Do you live in a long-term care facility, nursing home, or homeless shelter?",
+        ch: ["Yes", "No"],
+        a: "Yes"
+    },
+    {
+        q: "In the last two weeks, have you worked or volunteered in a healthcare facility or as a first responder? Healthcare facilities include a hospital, other medical setting (including dental care setting), or long-term care facility",
+        ch: ["Yes", "No"],
+        a: "Yes"
+    }
+]
