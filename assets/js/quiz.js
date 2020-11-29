3ea6e103a98278e1b49afc529a8f8ec40920817b
Gabriela Zalta <gzalta96@gmail.com>
2020-11-29 11:16:31 -0500

    series of questions created and ready to be displayed

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,6 +1,5 @@
 //quiz questions
-var questions= [
-    {
+var questions = [{
         q: "Are you experiencing any of the following symptoms: severe difficulty breathing, severe chest pain, feeling confused or unsire of where you are, losing consciousness ",
         ch: ["Yes", "No"],
         a: "Yes"
@@ -31,3 +30,172 @@ var questions= [
         a: "Yes"
     }
 ]
+
+//variables
+var current = 0;
+var answer = document.querySelectorAll(".answer");
+var question = document.getElementById("#quiz-q");
+var healthScore = 0;
+
+//display next question
+function nextQuestion(event) {
+    document.querySelector("#choices").classList.addClass("hide");
+}
+
+//display if the user is not suspected to have COVID-19
+function allOk(event) {
+
+}
+
+//display if the user is suspected of having COVID-19
+function getHelp(event) {
+
+}
+
+//display series of questions
+function startQuiz(event) {
+
+button.addEventListener("click", function (event) {
+
+    //display 1st question
+    var userChoice = questions[0].q;
+
+    question.innerHTML = userChoice;
+    document.querySelector("#choices").classList.remove("hide");
+
+    document.querySelector("#yes").innerHTML = questions[current].ch[0]
+    document.querySelector("#no").innerHTML = questions[current].ch[1]
+
+    
+    if (event.target.innerHTML === questions[0].a) {
+
+        getHelp();
+    } else {
+
+        //display 2nd question
+        var userChoice = questions[1].q;
+
+        question.innerHTML = userChoice;
+        document.querySelector("#choices").classList.remove("hide");
+
+        document.querySelector("#yes").innerHTML = questions[current].ch[0]
+        document.querySelector("#no").innerHTML = questions[current].ch[1]
+    }
+
+    nextQuestion();
+
+    if (event.target.innerHTML === questions[1].a) {
+
+        //display 3rd question
+        healthScore++;
+        console.log(healthScore);
+
+        var userChoice = questions[2].q;
+
+        question.innerHTML = userChoice;
+        document.querySelector("#choices").classList.remove("hide");
+
+        document.querySelector("#yes").innerHTML = questions[current].ch[0]
+        document.querySelector("#no").innerHTML = questions[current].ch[1]
+        
+    } else {
+
+        allOk();
+        
+    }
+
+    nextQuestion();
+
+    if (event.target.innerHTML === questions[2].a) {
+
+        //display 4th question and increase healthScore
+        healthScore++;
+        console.log(healthScore);
+
+        var userChoice = questions[3].q;
+
+        question.innerHTML = userChoice;
+        document.querySelector("#choices").classList.remove("hide");
+
+        document.querySelector("#yes").innerHTML = questions[current].ch[0]
+        document.querySelector("#no").innerHTML = questions[current].ch[1]
+        
+    } else {
+
+        //display 4th question
+        var userChoice = questions[3].q;
+
+        question.innerHTML = userChoice;
+        document.querySelector("#choices").classList.remove("hide");
+
+        document.querySelector("#yes").innerHTML = questions[current].ch[0]
+        document.querySelector("#no").innerHTML = questions[current].ch[1]
+    }
+
+    nextQuestion();
+
+    if (event.target.innerHTML === questions[3].a) {
+
+        //display 5th question and increase healthScore
+        healthScore++;
+        console.log(healthScore);
+
+        var userChoice = questions[4].q;
+
+        question.innerHTML = userChoice;
+        document.querySelector("#choices").classList.remove("hide");
+
+        document.querySelector("#yes").innerHTML = questions[current].ch[0]
+        document.querySelector("#no").innerHTML = questions[current].ch[1]
+
+    } else {
+
+        //display 5th question
+        var userChoice = questions[4].q;
+
+        question.innerHTML = userChoice;
+        document.querySelector("#choices").classList.remove("hide");
+
+        document.querySelector("#yes").innerHTML = questions[current].ch[0]
+        document.querySelector("#no").innerHTML = questions[current].ch[1]
+    }
+
+    nextQuestion();
+
+    if (event.target.innerHTML === questions[4].a) {
+
+        //display 6th question and increase healthScore
+        healthScore++;
+        console.log(healthScore);
+
+        var userChoice = questions[5].q;
+
+        question.innerHTML = userChoice;
+        document.querySelector("#choices").classList.remove("hide");
+
+        document.querySelector("#yes").innerHTML = questions[current].ch[0]
+        document.querySelector("#no").innerHTML = questions[current].ch[1]
+
+    } else {
+
+        //display 6th question
+        var userChoice = questions[5].q;
+
+        question.innerHTML = userChoice;
+        document.querySelector("#choices").classList.remove("hide");
+
+        document.querySelector("#yes").innerHTML = questions[current].ch[0]
+        document.querySelector("#no").innerHTML = questions[current].ch[1]
+    }
+
+    nextQuestion();
+
+    //if health score if more than 1, run getHelp function, otherwise, run allOk function
+    if (healthScore > 0) {
+        getHelp();
+    } else {
+        allOk();
+    }
+    
+})
+}
