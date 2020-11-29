7cc5b456a86c21e43be0ba4b3ac30ed5ce351d09
jconeff <jessicaconeff@gmail.com>
2020-11-29 01:06:24 -0500

    java added pos values

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -20,3 +20,43 @@ $("path, circle").hover(function(e) {
       return false;
     });
   }
+
+
+  
+  //map.addEventListener('path') 
+  
+
+  const stateApi = 'https://api.covidtracking.com/v1/states/current.json';
+    fetch(stateApi)
+    .then(response =>{
+        return response.json();
+    })
+    .then(stateData => {
+        console.log(stateData);
+
+        let hi = document.querySelector('#HI');
+        hi.setAttribute('data-info', stateData[0].positive)
+      
+
+        
+        console.log(stateData[0].positive);
+
+//start of loop
+
+var apiState;
+
+for (apiState of stateData) {
+    console.log(apiState);
+    let map =document.getElementById(apiState.state);
+    if (map !== null){
+
+
+
+
+        map.setAttribute('data-info',"Positive:" + apiState.positive);
+    }
+
+    
+}
+
+    });
