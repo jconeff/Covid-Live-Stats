4ddbf5ccfe0835138ff7bf16e3114238af4716e0
jconeff <jessicaconeff@gmail.com>
2020-11-29 03:37:27 -0500

    color coded and moment.js

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,4 @@
+//US-Map function
 $("path, circle").hover(function(e) {
     $('#info-box').css('display','block');
     $('#info-box').html($(this).data('info'));
@@ -20,43 +21,74 @@ $("path, circle").hover(function(e) {
       return false;
     });
   }
+  //End of US-Map Function
 
 
-  
-  //map.addEventListener('path') 
-  
+  //Covid stat API
+    const stateApi = 'https://api.covidtracking.com/v1/states/current.json';
+        fetch(stateApi)
+        .then(response =>{
+            return response.json();
+        })
+        .then(stateData => {
+            console.log(stateData);
+    //End of API call
 
-  const stateApi = 'https://api.covidtracking.com/v1/states/current.json';
-    fetch(stateApi)
-    .then(response =>{
-        return response.json();
-    })
-    .then(stateData => {
-        console.log(stateData);
 
-        let hi = document.querySelector('#HI');
-        hi.setAttribute('data-info', stateData[0].positive)
-      
-
-        
-        console.log(stateData[0].positive);
-
+    let currentDate = document.querySelector('.today');
+    currentDate.textContent = moment().format('LL');
 //start of loop
 
 var apiState;
 
+//math function
+var totalCases = 0;
+
+for (apiState of stateData){
+    totalCases = totalCases + apiState.positiveIncrease;
+
+}
+var average = totalCases/50;
+var topAverage = average *1.5;
+var bottomAverage = average/1.5
+
+// End of math function
+
 for (apiState of stateData) {
     console.log(apiState);
     let map =document.getElementById(apiState.state);
+    //Excluded outside states
     if (map !== null){
+        //Least amount of daily cases
+            if (bottomAverage >=apiState.positiveIncrease){ 
+                map.setAttribute('fill','#fcae62');
+            }
+            //Lesser amount of daily cases
+        if (bottomAverage <=apiState.positiveIncrease){
+            map.setAttribute('fill','#fa983a');
+        }
+    
+        //High amount of cases
+        if (average <= apiState.positiveIncrease){
+            map.setAttribute('fill','#e55039');
+        
+        }
+        //Highest amount of cases
+        if (topAverage <=apiState.positiveIncrease){
+            map.setAttribute('fill','#f02f05');
+        }
 
-
-
-
-        map.setAttribute('data-info',"Positive:" + apiState.positive);
+        //Daily positive cases
+        map.setAttribute('data-info',"Positive:" + apiState.positiveIncrease);
+        //map.setAttribute('data-info',"Deaths:" + apiState.deathIncrease);
+        
+        
     }
 
     
+
+    
 }
+//End of Loop
 
     });
