d7960b59d7767f638f0d1136263914528a06753d
jconeff <jessicaconeff@gmail.com>
2020-12-09 01:26:32 -0500

    local storage added, comparison between states

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -23,6 +23,24 @@ $("path, circle").hover(function(e) {
   }
   //End of US-Map Function
 
+  //saves when refreshed
+  //right side save
+let side1 = 1;
+localStorage.getItem('statename')
+localStorage.getItem('stateinfo')
+let statename =document.querySelector('.state1')
+let stateinfo =document.querySelector('.poscomp1')
+statename.textContent = localStorage.getItem('statename')
+stateinfo.textContent = localStorage.getItem('stateinfo')
+
+//Left side save
+localStorage.getItem('statename2')
+localStorage.getItem('stateinfo2')
+let statename2 =document.querySelector('.state2')
+let stateinfo2 =document.querySelector('.poscomp2')
+statename2.textContent = localStorage.getItem('statename2')
+stateinfo2.textContent = localStorage.getItem('stateinfo2')
+
 
   //Covid stat API
     const stateApi = 'https://api.covidtracking.com/v1/states/current.json';
@@ -34,9 +52,10 @@ $("path, circle").hover(function(e) {
             console.log(stateData);
     //End of API call
 
-
+    //Today's date
     let currentDate = document.querySelector('.today');
     currentDate.textContent = moment().format('LL');
+    //End of todays date
 //start of loop
 
 var apiState;
@@ -70,6 +89,46 @@ for (apiState of stateData) {
 
     //Excluded outside states
     if (map !== null){
+
+//event when state is clicked, comparison to right and left side
+
+        map.addEventListener('click', (event)=>{
+            //right side of page
+            if (side1 == 1) {
+                //Local Storage
+                localStorage.setItem("statename",event.target.id)
+                localStorage.setItem('stateinfo',event.target.getAttribute('data-info'))
+
+                
+                event.target
+                
+                let statename =document.querySelector('.state1')
+                let poscomp =document.querySelector('.poscomp1')
+                
+                statename.textContent = event.target.id;
+                poscomp.textContent = event.target.getAttribute('data-info');
+                side1 = 2
+               
+            }
+            //left side of page
+            else if(side1 == 2){
+
+                 localStorage.setItem("statename2",event.target.id)
+                localStorage.setItem('stateinfo2',event.target.getAttribute('data-info'))
+                
+                let statename2 =document.querySelector('.state2')
+                let poscomp2 =document.querySelector('.poscomp2')
+                statename2.textContent = event.target.id;
+                poscomp2.textContent = event.target.getAttribute('data-info');
+                    
+            }
+            
+        })
+
+        //end of event click
+
+
+       
         //Least amount of daily cases
             if (bottomAverage >=apiState.positiveIncrease){ 
                 map.setAttribute('fill','#fcae62');
@@ -101,13 +160,13 @@ for (apiState of stateData) {
         apiState.deathIncrease = numberWithCommas(apiState.deathIncrease)
         apiState.hospitalizedIncrease = numberWithCommas(apiState.hospitalizedIncrease)
 
-        //end of creat comma
+        //end of create comma
 
         //Daily positive cases
         map.setAttribute('data-info',['Positive: ' + apiState.positiveIncrease,'Deaths: '+apiState.deathIncrease,'Hospitalized: '+apiState.hospitalizedIncrease])
-        totalpos.textContent = 'Total New Daily Positive Cases: ' + totalCases;
-        totaldeath.textContent = 'Total New Daily Deaths: ' + totalDeaths;
-        totalhosp.textContent = 'Total New Daily Hospitalized: ' + totalHospital;
+        totalpos.textContent = ' Daily Positive Cases: ' + totalCases;
+        totaldeath.textContent = ' Daily Deaths: ' + totalDeaths;
+        totalhosp.textContent = ' Daily Hospitalized: ' + totalHospital;
        
         
         
